8f45d9ea8188de904f95e309ba7d3fac9703568b
victorGVC <victorstudy17@hotmail.com>
2022-03-01 11:51:37 -0300

    structure change that allows multi backends

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = require('express')()
 const consign = require('consign')
 
-consign().then('./config/middlewares.js')
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
 
 app.listen(3000, () => {
