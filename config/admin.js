e6cc91b2d3ffe6ee37cf3ac3cd47786f1f552069
victorGVC <victorstudy17@hotmail.com>
2022-03-07 14:31:38 -0300

    Access control done

---
+++ config/admin.js
@@ -0,0 +1,8 @@
+module.exports = middleware => {
+    return (req, res, next) => {
+        if(req.user.admin)
+            middleware(req, res, next)
+        else
+            res.status(401).send('action authorized for admin only')
+    }
+}
